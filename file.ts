4f4afc8f48c0e1c87b33a26d64f71cc2065364d6
Katia <katy25katy25katy@gmail.com>
2024-02-06 16:32:01 +1000

    blabla

---
+++ file.ts
@@ -0,0 +1,27 @@
+//const a: number = 1;
+//const b: string = 'cat';
+//const c: any[] = [1, 2, 3];
+//const v: boolean = true;
+
+//function f() {
+//    console.log('artur ieblan')
+//};
+
+//console.log(typeof a, typeof b, typeof c, typeof v, typeof f);
+
+//f();
+//const f:() => void = function f() {};
+
+
+function arturLochara(a) {
+if (a < 1) 
+return "a"
+else return "c"
+};
+
+arturLochara(123);
+
+
+
+
+
